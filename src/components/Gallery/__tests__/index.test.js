464766423af3b6793be4a8fbd1c794eeb7b09c51
Aphillip22 <amber.rain.phillips@gmail.com>
2021-12-16 01:07:03 -0700

    write and pass gallery/photolist tests

---
+++ src/components/Gallery/__tests__/index.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Gallery from '..'
+
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
+
+afterEach(cleanup)
+
+describe('Gallery is rendering', () => {
+    //it appears
+  it('renders', () => {
+    render(<Gallery currentCategory={portrait} />);
+  });
+
+  //snapshot test
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Gallery currentCategory={portrait} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  //h1 titled correctly
+  it('renders', () => {
+    const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+    expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+  })
+})
+
