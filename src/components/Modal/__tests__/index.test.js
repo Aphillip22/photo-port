0628b178e6ad0dd87aba5adf55f8872a4e6516c2
Aphillip22 <amber.rain.phillips@gmail.com>
2021-12-16 02:33:40 -0700

    complete tests for modal

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+afterEach(cleanup);
+
+describe('Modal is rendering', () => {
+    //baseline
+    it('renders', () => {
+        render(<Modal/>);
+    });
+    //snapshot
+})
